2297393df665225aeedbf3628dc461592f008e63
Youpeng <mofankk@gmail.com>
2021-07-04 18:43:52 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Hello, world!");
-    guess_game();
+    // guess_game();
+    ownership();
 }
 
 fn guess_game() {
@@ -40,5 +41,39 @@ fn guess_game() {
             }
         }
     }
+}
 
+fn ownership() {
+    let s1 = String::from("hello");
+    takes_ownership(s1);    // s1的值移动到函数里
+    // println!("{}", s1);            // s1的值在这里不再有效
+
+    let x = 5;
+    makes_copy(5);  // x的值复制到函数里
+    println!("{}", x);         // x在这里仍然有效
+
+    let s2 = String::from("world");
+    copy_ownership(s2.clone());     // 把s2复制一份到函数里
+    println!("{}", s2);                       // 所以s2在这里仍然有效
+
+    let (s3, length) = calculate_length(s2);
+    println!("{},{}", s3, length);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+fn copy_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
 }
